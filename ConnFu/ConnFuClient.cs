09cca3cfa3722bef26967a9ddb8ba1d66d7496ec
Gustavo García <gustavogb@gmail.com>
2011-11-01 14:41:24 +0100

    Initial commit

---
+++ ConnFu/ConnFuClient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace ConnFu
+{
+    public class ConnFuClient
+    {
+        public class TwitterEventArgs : EventArgs
+        {
+            public string From { get; set; }
+            public string Content { get; set; }
+        };
+        public delegate void TwitterHandler(object sender, TwitterEventArgs ev);
+        public event TwitterHandler Twitter;
+
+        public string Token { get; set; }
+
+        public ConnFuClient(string token)
+        {
+            Token = token;
+        }
+
+        private class ReadState
+        {
+            public ConnFuClient Client { get; set; }
+            public Stream Stream { get; set; }
+            public byte[] Buffer { get; set; }
+        }
+
+        public void Start()
+        {
+            var uri = "http://www.google.com";
+            var request = WebRequest.Create(uri);
+            var stream = request.GetResponse().GetResponseStream();
+
+            var buffer = new byte[1024];
+            stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(ReadCallBack), new ReadState
+            {
+                Client = this,
+                Stream = stream,
+                Buffer = buffer
+            });
+        }
+
+        private static void ReadCallBack(IAsyncResult asyncResult)
+        {
+            var state = (ReadState)asyncResult.AsyncState;
+            var client = state.Client;
+            var stream = state.Stream;
+
+            int read = stream.EndRead(asyncResult);
+
+            client.Twitter(client, new TwitterEventArgs { From = "test", Content = "tweet" });
+        }
+    }
+}
---
+++ ConnFuConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConnFu;
+
+namespace ConnFuConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connFu = new ConnFuClient("TOKEN");
+            connFu.Twitter += (sender, ev) =>
+                {
+                    Console.WriteLine("Look! My dude {0} posted {1} to Twitter", ev.From, ev.Content);
+                };
+
+            connFu.Start();
+            Console.ReadLine();
+        }
+    }
+}
